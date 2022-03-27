5024c6363d37a4fd672732ec8602c640af7bde41
tamirys <tocadilis@hotmail.com>
2022-03-27 00:50:38 -0300

    heroku deploy

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require('path');
-const nomeApp = process.env.npm_package_name;
+const appName = angular-moments;
 const app = express();
 
-app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+app.use(express.static(`${__dirname}/dist/${appName}`));
 
 app.get('/*', (req, res) => {
-res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+res.sendFile(path.join(`${__dirname}/dist/${appName}/index.html`));
 });
 
 app.listen(process.env.PORT || 8080);
